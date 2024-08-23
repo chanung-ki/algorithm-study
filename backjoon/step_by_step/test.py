f5f824424310352a41276614137b30ff17d918a3
chanung-ki <117616574+chanung-ki@users.noreply.github.com>
2024-08-23 21:47:48 +0900

    2024.08.23

---
+++ backjoon/step_by_step/test.py
@@ -0,0 +1,49 @@
+# 11382번
+# numbers = input().split()
+# num1 = int(numbers[0])
+# num2 = int(numbers[1])
+# num3 = int(numbers[2])
+# print(num1 + num2 + num3)
+
+
+# 25314번
+# n = int(input())
+# answer = 'int'
+# for i in range(n//4):
+#     answer = 'long ' + answer
+# print(answer)
+
+
+# 27866번
+# word = input()
+# number = int(input())
+# print(word[number-1])
+
+
+# 2743번
+# word = input()
+# print(len(word))
+
+
+# 9086번
+# number = int(input())
+# word_list = [input() for _ in range(number)]
+
+# for word in word_list:
+#     if len(word) == 1:
+#         print(word+word)
+#     else:
+#         print(word[0]+word[-1])
+
+
+# 1152번
+# temp_str = input()
+# print(len(temp_str.split()))
+
+
+# 11718번
+# while True:
+#     try:
+#         print(input())
+#     except EOFError:
+#         break
